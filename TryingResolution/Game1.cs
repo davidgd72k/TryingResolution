dbdc3d76a7a6806543b326b90a542a30067349d8
davidgd72k <59517993+davidgd72k@users.noreply.github.com>
2021-01-12 21:06:04 +0100

    Agregar archivos de proyecto.

---
+++ TryingResolution/Game1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.IO;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace TryingResolution
+{
+    public class Game1 : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+
+        string[] data = new string[4];
+
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+
+            _graphics.PreferredBackBufferWidth = 640;
+            _graphics.PreferredBackBufferHeight = 480;
+            _graphics.IsFullScreen = false;
+            _graphics.ApplyChanges();
+
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here                        
+
+            data[0] = _graphics.PreferredBackBufferWidth.ToString();
+            data[1] = _graphics.PreferredBackBufferHeight.ToString();
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            // TODO: use this.Content to load your game content here
+        }
+
+        protected override void UnloadContent()
+        {
+            data[2] = _graphics.PreferredBackBufferWidth.ToString();
+            data[3] = _graphics.PreferredBackBufferHeight.ToString();
+
+            StreamWriter outDebugFile = new StreamWriter("size_result.txt");
+
+            foreach(string line in data)
+            {
+                outDebugFile.WriteLine(line);
+            }
+
+            outDebugFile.Close();
+
+            base.UnloadContent();
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            
+            var keystate = Keyboard.GetState();
+
+            if (keystate.IsKeyDown(Keys.F11))
+            {
+                _graphics.ToggleFullScreen();
+            }
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            // TODO: Add your drawing code here
+
+            base.Draw(gameTime);
+        }
+    }
+}
